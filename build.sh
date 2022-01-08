43e65670839b2cc7069c3a60318ab51dfaed00c2
Khizer Syed <khizerrocks.demon@gmail.com>
2022-01-07 20:51:26 -0500

    Updated code

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo "Build Date - $build_date"
 
 # Delete build folder
 echo "**************** Preparing for build *******************"
-rm -rf build
+rm -rf iffcargo
 echo "Cleanup complete"
 
 # Build server & client
@@ -15,16 +15,16 @@ echo "Build succeeded"
 
 # copy server, package.json and package-lock.json to build folder
 echo "**************** Copy package.json and package-lock.json file *******************"
-cp package.json package-lock.json build
-cp -R server build
+cp package.json package-lock.json iffcargo
+cp -R server iffcargo
 echo "Copied Files"
 
-echo "File size before compression: $(du -sh build)"
+echo "File size before compression: $(du -sh iffcargo)"
 
 # zip folder
 echo "**************** Compressing build folder *******************"
-build_artifact="build-"$build_date.zip
-zip -r "$build_artifact" build
+build_artifact="iffcargo-"$build_date.zip
+zip -r "$build_artifact" iffcargo
 echo "File size after compression: $(du -sh "$build_artifact")"
 
 
